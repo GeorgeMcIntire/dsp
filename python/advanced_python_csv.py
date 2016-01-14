cada8d83ac2c63d7799e48ead4397d1f0e914ffd
GeorgeMcIntire <geo.mcintire@gmail.com>
2016-01-14 12:36:29 -0800

    Answers

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,26 @@
-PLACE YOUR CODE HERE
+a = open("/Users/georgemcintire/ds/metis/prework/dsp/python/faculty.csv", "r")
+b = open("/Users/georgemcintire/ds/metis/prework/dsp/python/emails.csv", "w")
+
+
+f = a.read()
+
+
+w = f.split("\n")
+
+faculty = []
+
+for i in w:
+	split_row = i.split(",")
+	faculty.append(split_row)
+
+#print faculty
+
+
+emails = []
+
+for i in faculty[1:]:
+	emails.append(i[3])
+
+import csv 
+wr = csv.writer(b, quoting=csv.QUOTE_ALL)
+wr.writerow(emails)
